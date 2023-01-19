191774f3e6a3af6fd089123db417fda1c8519d87
TheoB <theo.boulinguez@le-campus-numerique.fr>
2023-01-19 10:44:53 +0100

    first commit

---
+++ squareGamesSpring/src/main/java/com/example/squaregamesspring/SquareGamesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.squaregamesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesSpringApplication.class, args);
+	}
+
+}
